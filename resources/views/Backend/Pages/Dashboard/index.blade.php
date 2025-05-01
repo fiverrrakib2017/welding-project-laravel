f37199b3de1dc4ff212b0d68c30961c93e129fb6
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-05-01 21:42:45 +0600

    user role some changes

--- resources/views/Backend/Pages/Dashboard/index.blade.php
+++ resources/views/Backend/Pages/Dashboard/index.blade.php
@@ -7,13 +7,36 @@
   <div class="row" id="dashboardCards">
 
     <div class="col-lg-3 col-6 card-item wow animate__animated animate__fadeInUp" data-wow-delay="0.5s">
-        <div class="small-box bg-primary">
+        <div class="small-box bg-success">
             <div class="inner">
-                <h3>02</h3>
-                <p>Pending Tickets</p>
+                <h3>
+                    @php
+                     $total_student = App\Models\Student::count();
+
+                    @endphp
+                    {{ $total_student }}
+                </h3>
+                <p>Total Students</p>
             </div>
             <div class="icon">
-                <i class="fas fas fa-solid fa-exclamation-triangle fa-2x text-gray-300"></i>
+                <i class="fas fas fa-solid fa-user-graduate fa-2x text-gray-300"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6 card-item wow animate__animated animate__fadeInUp" data-wow-delay="0.5s">
+        <div class="small-box bg-primary">
+            <div class="inner">
+                <h3>
+                    @php
+                     $total_course = App\Models\Course::count();
+
+                    @endphp
+                    {{ $total_course }}
+                </h3>
+                <p>Total Course</p>
+            </div>
+            <div class="icon">
+                <i class="fas fas fa-solid fa-user-graduate fa-2x text-gray-300"></i>
             </div>
         </div>
     </div>
